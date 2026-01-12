7d1f354784a730c5a5986ec122ccbe9d26af378b
Zikkjj <salatielbs16@gmail.com>
2026-01-12 20:08:48 -0300

    usado os imports

--- src/fundamentos/Import.java
+++ src/fundamentos/Import.java
@@ -1,5 +1,21 @@
 package fundamentos;
 
-public class Import {
 
+/* Os imports são buscados fora da classe*/
+import java.util.Date; // pacote importando dentro dos pacotes java
+
+
+public class Import {
+	public static void main(String[] args) {
+		String s = "Bom dia";
+		System.out.println(s);
+		
+		java.lang.String b = "Boa tarde!";
+		System.out.println(b);
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+//		JButton botao = new JButton();
+	}
 }
